f3ed3a73609f29e4754a3eb05292708b63e021ba
Artem Pavlov <pavlov329811@gmail.com>
2021-02-03 11:46:50 +0300

    remove swagger

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,14 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@ApiSecurity('basic')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @ApiTags('app')
   getHello(): string {
     return "Start";
   }
